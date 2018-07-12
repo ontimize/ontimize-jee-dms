dbb665200043182f67a2ecbe18b90febec0f428f
manuel.cid <manuel.cid@imatia.com>
2018-07-12 16:10:47 +0000

    Add rest method 'fileUpdate'
    git-svn-id: https://svn.imatia.com/ontimize/ontimize-jee-dms/trunk@3872 c65a67e3-ea31-493c-9319-7ebef4ec2fa5

--- ontimize-jee-dms-rest/src/main/java/com/ontimize/jee/server/dms/rest/DMSRestController.java
+++ ontimize-jee-dms-rest/src/main/java/com/ontimize/jee/server/dms/rest/DMSRestController.java
@@ -34,21 +34,22 @@ import org.springframework.web.multipart.MultipartFile;
 import com.ontimize.db.EntityResult;
 import com.ontimize.gui.SearchValue;
 import com.ontimize.jee.common.exceptions.DmsException;
+import com.ontimize.jee.common.naming.DMSNaming;
 import com.ontimize.jee.common.services.dms.DMSCategory;
 import com.ontimize.jee.common.services.dms.DocumentIdentifier;
 import com.ontimize.jee.common.services.dms.IDMSService;
-import com.ontimize.jee.common.tools.CheckingTools;
 import com.ontimize.jee.server.dms.model.OFile;
 import com.ontimize.jee.server.rest.FileListParameter;
 import com.ontimize.jee.server.rest.InsertParameter;
 import com.ontimize.jee.server.rest.QueryParameter;
+import com.ontimize.jee.server.rest.UpdateFileParameter;
 
 public abstract class DMSRestController<T extends IDMSService, N extends IDMSNameConverter> {
 
 	protected static final String	CATEGORY_FILTER_KEY	= "FM_FOLDER_PARENT_KEY";
 
 	/** The logger. */
-	private final static Logger		logger				= LoggerFactory.getLogger(DMSRestController.class);
+	private static final Logger		logger				= LoggerFactory.getLogger(DMSRestController.class);
 
 	public abstract T getService();
 
@@ -105,7 +106,7 @@ public abstract class DMSRestController<T extends IDMSService, N extends IDMSNam
 	 *            the file identifier
 	 * @param response
 	 */
-	@RequestMapping(path = "/getFile/{fileId}", method = RequestMethod.GET, produces = " application/octet-stream")
+	@RequestMapping(path = "/getFile/{fileId}", method = RequestMethod.GET, produces = "application/octet-stream")
 	public @ResponseBody void fileGetContent(@PathVariable("fileId") Integer fileId, HttpServletResponse response) {
 		InputStream fis = null;
 		try {
@@ -183,7 +184,7 @@ public abstract class DMSRestController<T extends IDMSService, N extends IDMSNam
 	 *            the file name
 	 * @param response
 	 */
-	@RequestMapping(path = "/getZipFile/{file}", method = RequestMethod.GET, produces = " application/octet-stream")
+	@RequestMapping(path = "/getZipFile/{file}", method = RequestMethod.GET, produces = "application/octet-stream")
 	public @ResponseBody void fileGetZip(@PathVariable("file") String file, HttpServletResponse response) {
 		InputStream fis = null;
 		File zipFile = null;
@@ -233,7 +234,7 @@ public abstract class DMSRestController<T extends IDMSService, N extends IDMSNam
 				av.put(this.dmsNameConverter.getCategoryIdColumn(), folderId);
 			}
 			DocumentIdentifier id = this.getService().fileInsert(workspaceId, av, is);
-			return new ResponseEntity<DocumentIdentifier>(id, HttpStatus.OK);
+			return new ResponseEntity<>(id, HttpStatus.OK);
 		} catch (Exception e) {
 			DMSRestController.logger.error("{}", e.getMessage(), e);
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -250,8 +251,6 @@ public abstract class DMSRestController<T extends IDMSService, N extends IDMSNam
 	 */
 	@RequestMapping(value = "/deleteFiles/{workspaceId}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Void> delete(@PathVariable("workspaceId") Integer workspaceId, @RequestBody FileListParameter deleteParameter) throws DmsException {
-		Object service = this.getService();
-		CheckingTools.failIf(service == null, NullPointerException.class, "Service is null");
 		try {
 			List<OFile> fileList = deleteParameter.getFileList();
 			for (OFile file : fileList) {
@@ -271,6 +270,8 @@ public abstract class DMSRestController<T extends IDMSService, N extends IDMSNam
 	}
 
 	/**
+	 * Inserts a folder in the workspace with the provided work space identifier
+	 * 
 	 * @param workspaceId
 	 *            the work space identifier
 	 * @param name
@@ -280,7 +281,6 @@ public abstract class DMSRestController<T extends IDMSService, N extends IDMSNam
 	 */
 	@RequestMapping(path = "/insertFolder/{workspaceId}/{name}", method = RequestMethod.POST)
 	public ResponseEntity<Void> folderInsert(@PathVariable("workspaceId") Integer workspaceId, @PathVariable("name") String name, @RequestBody InsertParameter insertParam) {
-
 		try {
 			Map<Object, Object> data = insertParam.getData();
 			Object categoryId = null;
@@ -288,10 +288,35 @@ public abstract class DMSRestController<T extends IDMSService, N extends IDMSNam
 				categoryId = data.remove(DMSRestController.CATEGORY_FILTER_KEY);
 			}
 			this.getService().categoryInsert(workspaceId, name, (Serializable) categoryId, null);
-			return new ResponseEntity<Void>(HttpStatus.OK);
+			return new ResponseEntity<>(HttpStatus.OK);
 		} catch (Exception e) {
 			DMSRestController.logger.error("{}", e.getMessage(), e);
-			return new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@RequestMapping(path = "/fileUpdate", method = RequestMethod.POST)
+	public ResponseEntity<Void> fileUpdate(@RequestBody UpdateFileParameter updateFileParameter) {
+		try {
+			OFile file = updateFileParameter.getFile();
+			if (file != null) {
+				Map<Object, Object> data = updateFileParameter.getParams();
+				Map<String, Object> av = new HashMap<>();
+				if (data.containsKey("name")) {
+					if (file.isDirectory()) {
+						av.put(DMSNaming.CATEGORY_CATEGORY_NAME, data.get("name"));
+						this.getService().categoryUpdate(file.getId(), av);
+					} else {
+						av.put(DMSNaming.DOCUMENT_FILE_NAME, data.get("name"));
+						this.getService().fileUpdate(file.getId(), av, this.getService().fileGetContent(file.getId()));
+					}
+					return new ResponseEntity<>(HttpStatus.OK);
+				}
+			}
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} catch (Exception e) {
+			DMSRestController.logger.error("{}", e.getMessage(), e);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
@@ -360,13 +385,13 @@ public abstract class DMSRestController<T extends IDMSService, N extends IDMSNam
 	 * @throws DmsException
 	 */
 	protected List<OFile> getCategoryFiles(Integer documentId, Integer categoryId, boolean includeSubCategories) throws DmsException {
-		List<OFile> files = new ArrayList<OFile>();
+		List<OFile> files = new ArrayList<>();
 
 		DMSCategory categories = this.getService().categoryGetForDocument(documentId, this.getCategoryColumns(null));
 		this.getCategoriesAsFiles(categories, categoryId, files, includeSubCategories);
 
-		Map<Object, Object> filter = new HashMap<Object, Object>();
-		List<Object> categoriesIds = new ArrayList<Object>();
+		Map<Object, Object> filter = new HashMap<>();
+		List<Object> categoriesIds = new ArrayList<>();
 		categoriesIds.add(categoryId);
 		if (includeSubCategories) {
 			for (OFile file : files) {
